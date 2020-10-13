20da48c9ec6a68e14a37e78e3e8085a04ca8b408
ruby73 <ouueyeb1@gmail.com>
2020-10-13 13:38:00 +0900

    13:37

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -4,20 +4,20 @@ import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 
 const Main = () => <>
         <BrowserRouter>
-            <div className= "main">
+            <div className="main">
                 <Switch>
-                    <Route exect path="/" component={Home}/>
+                    <Route exact path="/" component={Home}/>
                     <Route path="/about" component={About}/>
                     <Redirect from={"/history"} to={"/about/history"}/>
-                    <Redirect from={"/service"} to={"/about/service"}/>
+                    <Redirect from={"/services"} to={"/about/services"}/>
                     <Redirect from={"/location"} to={"/about/location"}/>
-                    <Route path="/contact" component={Contact}/>
+                    <Route path="contact" component={Contact}/>
                     <Route path="/events" component={Events}/>
                     <Route path="/products" component={Products}/>
                     <Route component={Error}/>
                 </Switch>
             </div>
+
         </BrowserRouter>
     </>
-
 export default Main
