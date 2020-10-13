64710b0d1d8a2e681cdd0e5ff6dec81637de8976
ruby73 <ouueyeb1@gmail.com>
2020-10-13 11:04:48 +0900

    11:04

---
+++ src/pages/Main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import {About, Contact, Events, Home, Products, Error} from '../components'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+
+const Main = () => <>
+
+    </>
+
+export default Main
